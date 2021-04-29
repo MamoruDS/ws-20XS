2042e260652c359a4448692b966010b8d1fd8f13
MamoruDS <mamoruds.io@gmail.com>
2021-04-29 19:28:56 +0900

    added: jsonc.py for json with comments support

---
+++ textmate/theme/jsonc.py
@@ -0,0 +1,16 @@
+import re
+
+
+def to_json(input: str) -> str:
+    RE_CMT = re.compile(
+        r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)",
+        re.MULTILINE | re.DOTALL,
+    )
+    RE_COMMA = re.compile(r",([\s|\n]{1,}\})")
+    try:
+        res = RE_CMT.sub(r"\1", input)
+        res = RE_COMMA.sub(r"\1", res)
+        return res
+    except Exception as e:
+        # TODO:
+        raise e
